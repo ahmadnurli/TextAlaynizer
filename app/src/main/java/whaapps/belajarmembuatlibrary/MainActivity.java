163ea6a911780c4853403db1fb6c38cfd894613a
ahmadnurli <anurli264@gmail.com>
2018-04-29 15:20:10 +0700

    i

---
+++ app/src/main/java/whaapps/belajarmembuatlibrary/MainActivity.java
@@ -0,0 +1,23 @@
+package whaapps.belajarmembuatlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.textalay.AlayHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String text = "Reno pergi ke Bali";
+        String textalay = AlayHelper.alaynize(text);
+
+        TextView textView = findViewById(R  .id.textView);
+        textView.setText(textalay);
+
+    }
+}
