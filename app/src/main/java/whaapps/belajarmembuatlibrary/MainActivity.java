86ec9874b76bd32747aa2ed1b9275a8eb70b127b
ahmadnurli <anurli264@gmail.com>
2018-04-29 14:07:55 +0700

    latihan labrary

---
+++ app/src/main/java/whaapps/belajarmembuatlibrary/MainActivity.java
@@ -0,0 +1,18 @@
+package whaapps.belajarmembuatlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String text = "Reno pergi ke Bali";
+        String textalay = AlayHelper.analize(text);
+
+
+    }
+}
